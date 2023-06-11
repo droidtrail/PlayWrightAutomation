7070a8e36b6e2f4860092bea6ab8e4b8af1bbad7
Leandro Pereira <leandro.nares@gmail.com>
2023-06-11 13:52:22 -0300

    8. What is browser context and Page fixtures in Playwright ? - Example

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,5 +1,16 @@
 const {test} = require('@playwright/test')
 
-test('First Playwright test', async ()=>{
+test('Browser Context Playwright test', async ({browser})=>{
+
+    //chrome - plugins/cookies
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+})
+
+test('Page First Playwright test', async ({browser,page})=>{
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
 })
