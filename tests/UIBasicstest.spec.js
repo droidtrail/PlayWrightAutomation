6c7c08933095fbd50e9a39969fbe044c094ec240
Leandro Pereira <leandro.nares@gmail.com>
2023-06-12 15:51:13 -0300

    13. How to work with locators which extract multiple webelements in page

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,25 +1,32 @@
 const {test,expect} = require('@playwright/test');
 
 test.only('Browser Context Playwright test', async ({browser})=>{
-
-    //chrome - plugins/cookies
     const context = await browser.newContext();
     const page = await context.newPage();
+    const userName = page.locator('#username');
+    const password = page.locator("[type='password']");
+    const signIn = page.locator("#signInBtn");
+    
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
     //css
-    await page.locator('#username').type("rahulshetty");
-    await page.locator("[type='password']").type("learning");
-    await page.locator("#signInBtn").click();
+    await userName.type("rahulshetty");
+    await password.type("learning");
+    await signIn.click();
+
     //await until this locator shown up page
     await page.locator("[style*='block']").textContent();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText('Incorrect username/password.');
+
+    //type - fill
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+    console.log(await page.locator(".card-body a").nth(0).textContent());
 });
-
 test('Page First Playwright test', async ({browser,page})=>{
-
     await page.goto("https://www.google.com/");
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
