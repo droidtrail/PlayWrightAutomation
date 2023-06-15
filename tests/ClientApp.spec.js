d11d1ded641d6512e6e3e722887730ca708ba19c
Leandro Pereira <leandro.nares@gmail.com>
2023-06-15 16:24:06 -0300

    24. Write the Script to dynamically find the product to buy from list of products

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,12 +1,21 @@
 const {test,expect} = require('@playwright/test');
 
 test('Browser Context Playwright test', async ({page})=>{
-    
+    const productName = 'Zara Coat 3';
+    const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill("leandro.pereiracr@gmail.com");
     await page.locator("#userPassword").fill("x85.2eRYwab6BY");
     await page.locator("[value='Login']").click();
     await page.waitForLoadState('networkidle');
-    const titles = await page.locator(".card-body a").allTextContents();
-    console.log(titles);
+    //Zara Coat 4
+    const count = await products.count();
+    for(let i=0; i < count; i++){
+        if (await products.nth(i).locator("b").textContent()=== productName)
+        {
+            //add to cart
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+    }
 });
