687c025daad647b8fe122a14934477dc786ab7e8
Leandro Pereira <leandro.nares@gmail.com>
2023-06-15 18:16:05 -0300

    25. Add assertions for the actions performed and implement necessary Sync steps

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,7 +1,7 @@
 const {test,expect} = require('@playwright/test');
 
 test('Browser Context Playwright test', async ({page})=>{
-    const productName = 'Zara Coat 3';
+    const productName = 'zara coat 3';
     const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill("leandro.pereiracr@gmail.com");
@@ -10,12 +10,18 @@ test('Browser Context Playwright test', async ({page})=>{
     await page.waitForLoadState('networkidle');
     //Zara Coat 4
     const count = await products.count();
-    for(let i=0; i < count; i++){
-        if (await products.nth(i).locator("b").textContent()=== productName)
+    for(let i=0; i <= count; i++)
+    {
+        if (await products.nth(i).locator("b").textContent() === productName)
         {
             //add to cart
             await products.nth(i).locator("text= Add To Cart").click();
             break;
         }
     }
+    await page.locator("[routerlink='/dashboard/cart']").click();
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator("h3:has-text('zara coat 3')").isVisible();
+    expect(bool).toBeTruthy();
+    // await page.pause();
 });
