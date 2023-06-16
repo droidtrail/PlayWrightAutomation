84c5ef12ce302597f1435df934c88490ca7493e9
Leandro Pereira <leandro.nares@gmail.com>
2023-06-16 15:48:05 -0300

    26. Handling Auto suggestive dropdown options with playwright - Example

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,6 +1,6 @@
-const {test,expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('Browser Context Playwright test', async ({page})=>{
+test('Browser Context Playwright test', async ({ page }) => {
     const productName = 'zara coat 3';
     const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
@@ -10,10 +10,8 @@ test('Browser Context Playwright test', async ({page})=>{
     await page.waitForLoadState('networkidle');
     //Zara Coat 4
     const count = await products.count();
-    for(let i=0; i <= count; i++)
-    {
-        if (await products.nth(i).locator("b").textContent() === productName)
-        {
+    for (let i = 0; i <= count; i++) {
+        if (await products.nth(i).locator("b").textContent() === productName) {
             //add to cart
             await products.nth(i).locator("text= Add To Cart").click();
             break;
@@ -23,5 +21,17 @@ test('Browser Context Playwright test', async ({page})=>{
     await page.locator("div li").first().waitFor();
     const bool = await page.locator("h3:has-text('zara coat 3')").isVisible();
     expect(bool).toBeTruthy();
-    // await page.pause();
+    await page.locator("button[type='button']").last().click();
+    await page.locator("[placeholder='Select Country']").type('Brazi', { delay: 100 });
+    const dropwnOptions = page.locator(".ta-results");
+    await dropwnOptions.waitFor();
+    const optionsCount = await dropwnOptions.locator("button").count();
+    for (let i = 0; i <= optionsCount; i++) {
+        const text = await dropwnOptions.locator("button").nth(i).textContent();
+        if (text === " Brazil") {
+            await dropwnOptions.locator("button").nth(i).click();
+            break;
+        }
+    }
+    await page.pause();
 });
