b20e4f39a190c897e6700652d7ad394da5eb138b
Leandro Pereira <leandro.nares@gmail.com>
2023-06-17 11:02:07 -0300

    28. Dynamically find the order from OrderHistory page using Playwright Script logic

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,10 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
 test('Browser Context Playwright test', async ({ page }) => {
+    const email = "leandro.pereiracr@gmail.com";
     const productName = 'zara coat 3';
     const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
-    await page.locator("#userEmail").fill("leandro.pereiracr@gmail.com");
+    await page.locator("#userEmail").fill(email);
     await page.locator("#userPassword").fill("x85.2eRYwab6BY");
     await page.locator("[value='Login']").click();
     await page.waitForLoadState('networkidle');
@@ -33,5 +34,25 @@ test('Browser Context Playwright test', async ({ page }) => {
             break;
         }
     }
-    await page.pause();
+    await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+    await page.locator(".action__submit").click();
+    await expect(page.locator(".hero-primary")).toHaveText(' Thankyou for the order. ');
+
+    const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
+    await expect(page.locator(".em-spacer-1 .ng-star-inserted")).toHaveText(orderId);
+    await page.locator("[routerlink*='myorders']").first().click();
+    await page.locator("tbody").waitFor();
+    
+    const rows = await page.locator("tbody tr");
+
+    for (let i = 0; i <= await rows.count(); ++i) {
+        const rowOrderId = await rows.nth(i).locator("[scope='row']").textContent();
+        if (orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator(".btn-primary").first().click();
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator("div  .col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    // await page.pause();
 });
