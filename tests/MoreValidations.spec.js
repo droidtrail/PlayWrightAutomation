f0bd6d5bb1ed2e950fb1e4983ec145c8a71a03e6
Leandro Pereira <leandro.nares@gmail.com>
2023-06-17 18:49:25 -0300

    30. How to automate Java/JavaScript Alert popups with Playwright

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -8,6 +8,13 @@ test("Popup validations",async ({page})=>{
     await expect (page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect (page.locator("#displayed-text")).toBeHidden();
+    // await page.pause();
+})
+test.only("How to automate Java/JavaScript Alert popups with Playwright",async({page})=>{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    page.on('dialog',dialog =>dialog.accept());
     await page.pause();
-
+    await page.locator("#confirmbtn").click();
+    await page.locator("#mousehover").hover();
+    await page.pause();
 })
