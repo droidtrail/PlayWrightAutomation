2978969dc351996ffd1f3cafd19d3e85caee21cf
Leandro Pereira <leandro.nares@gmail.com>
2023-06-17 21:17:58 -0300

    31. How to handle & Automate frames with Playwright - Example

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,20 +1,32 @@
-const { test,expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test("Popup validations",async ({page})=>{
+test("Popup validations", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     // await page.goto("https://www.google.com/");
     // await page.goBack();
     // await page.goForward();
-    await expect (page.locator("#displayed-text")).toBeVisible();
+    await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
-    await expect (page.locator("#displayed-text")).toBeHidden();
+    await expect(page.locator("#displayed-text")).toBeHidden();
     // await page.pause();
 })
-test.only("How to automate Java/JavaScript Alert popups with Playwright",async({page})=>{
+test("How to automate Java/JavaScript Alert popups with Playwright", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-    page.on('dialog',dialog =>dialog.accept());
+    page.on('dialog', dialog => dialog.accept());
     await page.pause();
     await page.locator("#confirmbtn").click();
     await page.locator("#mousehover").hover();
     await page.pause();
 })
+
+test.only("How to handle & Automate frames with Playwright", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    const framesPage = page.frameLocator("#courses-iframe"); // this will give new page object
+    await page.locator("#courses-iframe").scrollIntoViewIfNeeded();
+    await framesPage.locator("li a[href*='lifetime-access']:visible").click();
+    const textCheck = await framesPage.locator(".text h2").textContent();
+    await expect(textCheck.split(" ")[1]).toEqual('13,522');
+    console.log('Resultado >>>>>>> ' + textCheck.split(" ")[1]);
+})
+
+
