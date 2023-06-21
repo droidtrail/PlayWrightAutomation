62c550ac077d0737fb6f0129416889a979d40663
Leandro Pereira <leandro.nares@gmail.com>
2023-06-21 11:56:21 -0300

    36. End to end validation with mix of API & Web concepts - Reduce test time

--- tests/WebApiPart1.spec.js
+++ tests/WebApiPart1.spec.js
@@ -1,7 +1,9 @@
 const { test, expect, request } = require('@playwright/test');
 const loginPayLoad = { userEmail: "leandro.pereiracr@gmail.com", userPassword: "x85.2eRYwab6BY" };
+const orderPayLoad = {orders:[{country:"United States",productOrderedId:"6262e9d9e26b7e1a10e89c04"}]};
 let token;
-
+let orderId;
+//Login e obter o token via api.
 test.beforeAll(async () => 
 {
     const apiContext = await request.newContext();
@@ -13,7 +15,19 @@ test.beforeAll(async () =>
     expect(loginResponse.ok()).toBeTruthy();
     const loginResponseJson = await loginResponse.json();
     token = loginResponseJson.token;
-    console.log('Resposta >>>>>> ' + token);
+    console.log('orderId >>>>>> ' + token);
+    //Criando a ordem via api.
+    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+    {
+        data:orderPayLoad,
+        headers:{
+                    'Authorization': token,
+                    'Content-Type' : 'application/json'
+        },
+    })
+    const orderResponseJson = await orderResponse.json();
+    orderId = orderResponseJson.orders[0];
+    console.log('orderId >>>>>> ' + orderId);
 });
 
 test.beforeEach(() => {
@@ -21,53 +35,17 @@ test.beforeEach(() => {
 });
 
 test('Place the order', async ({ page }) => {
-
+    //Passando o token para o local storage.
     page.addInitScript(value => {
         window.localStorage.setItem('token',value);
 
     }, token );
-
+    //Acessando a página de ordens
     await page.goto("https://rahulshettyacademy.com/client");
-    const products = page.locator(".card-body");
-    const email = "leandro.pereiracr@gmail.com";
-    const productName = 'zara coat 3';
-    await page.waitForLoadState('networkidle');
-    //Zara Coat 4
-    const count = await products.count();
-    for (let i = 0; i <= count; i++) {
-        if (await products.nth(i).locator("b").textContent() === productName) {
-            //add to cart
-            await products.nth(i).locator("text= Add To Cart").click();
-            break;
-        }
-    }
-    await page.locator("[routerlink='/dashboard/cart']").click();
-    await page.locator("div li").first().waitFor();
-    const bool = await page.locator("h3:has-text('zara coat 3')").isVisible();
-    expect(bool).toBeTruthy();
-    await page.locator("button[type='button']").last().click();
-    await page.locator("[placeholder='Select Country']").type('Brazi', { delay: 100 });
-    const dropwnOptions = page.locator(".ta-results");
-    await dropwnOptions.waitFor();
-    const optionsCount = await dropwnOptions.locator("button").count();
-    for (let i = 0; i <= optionsCount; i++) {
-        const text = await dropwnOptions.locator("button").nth(i).textContent();
-        if (text === " Brazil") {
-            await dropwnOptions.locator("button").nth(i).click();
-            break;
-        }
-    }
-    await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
-    await page.locator(".action__submit").click();
-    await expect(page.locator(".hero-primary")).toHaveText(' Thankyou for the order. ');
-
-    const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
-    await expect(page.locator(".em-spacer-1 .ng-star-inserted")).toHaveText(orderId);
     await page.locator("[routerlink*='myorders']").first().click();
     await page.locator("tbody").waitFor();
-
+    //Busca pela ordem que foi criada via api
     const rows = await page.locator("tbody tr");
-
     for (let i = 0; i <= await rows.count(); ++i) {
         const rowOrderId = await rows.nth(i).locator("[scope='row']").textContent();
         if (orderId.includes(rowOrderId)) {
@@ -76,6 +54,6 @@ test('Place the order', async ({ page }) => {
         }
     }
     const orderIdDetails = await page.locator("div  .col-text").textContent();
+    await page.pause();
     expect(orderId.includes(orderIdDetails)).toBeTruthy();
-    // await page.pause();
 });
