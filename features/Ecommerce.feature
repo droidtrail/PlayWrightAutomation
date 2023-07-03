d0f030dfef014d5e7c51a0b7e328c9748c6b3054
Leandro Pereira <leandro.nares@gmail.com>
2023-07-03 14:46:42 -0300

    69. Create Step Definition files and associate with Cucumber.js config file

--- features/Ecommerce.feature
+++ features/Ecommerce.feature
@@ -1,8 +1,7 @@
 Feature: Feature name
-
-    Scenario: Placing the order
-      Given a login to Ecommerce application with "username" and "password"
-      When Add "zara coat 3" to Cart
-      Then Verify "zara coat 3" is displayed in Cart
-      When Enter valid details and place the Order
-      Then Verify order in present in the OrderHistory
+  Scenario: Placing the order
+    Given a login to Ecommerce application with "username" and "password"
+    When Add "zara coat 3" to Cart
+    Then Verify "zara coat 3" is displayed in Cart
+    When Enter valid details and place the Order
+    Then Verify order in present in the OrderHistory
---
+++ features/step_definitions/steps.js
@@ -0,0 +1,26 @@
+
+
+Given('a login to Ecommerce application with {username} and {password}', function (username, password) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('Add {string} to Cart', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Verify {string} is displayed in Cart', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('Enter valid details and place the Order', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Verify order in present in the OrderHistory', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
